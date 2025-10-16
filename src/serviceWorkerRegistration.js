f70ded034b99e788c05d29a2632885d2d397729a
LavacoMotors <Lavacomotors@gmail.com>
2025-10-16 12:05:43 -0700

    Update serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,1 +1,64 @@
+// serviceWorkerRegistration.js
+
+// This lets your app work offline and load faster (PWA support)
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl);
+      } else {
+        registerValidSW(swUrl);
+      }
+    });
+  }
+}
+
+function registerValidSW(swUrl) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      console.log('Service worker registered:', registration);
+    })
+    .catch(error => {
+      console.error('Error during service worker registration:', error);
+    });
+}
+
+function checkValidServiceWorker(swUrl) {
+  fetch(swUrl)
+    .then(response => {
+      if (response.status === 404) {
+        navigator.serviceWorker.ready.then(registration => {
+          registration.unregister();
+        });
+      } else {
+        registerValidSW(swUrl);
+      }
+    })
+    .catch(() => {
+      console.log('No internet connection. Running in offline mode.');
+    });
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then(registration => {
+        registration.unregister();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+}
 
